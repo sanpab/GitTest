1f75d1006f9035c88f94b0701f4b07f0ac293bb0
sanpab <sanpab@LOUL140393.verit.dnv.com>
2017-03-01 16:41:11 +0000

    Initial check in of HelloWorld v2

---
+++ src/com/myproject/HowAreYou.java
@@ -0,0 +1,10 @@
+package com.myproject;
+
+public class HowAreYou {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("How are you?");
+    }
+
+}
